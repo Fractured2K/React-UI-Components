fbcb443ec780c50c23265be33467ec0378003bfd
Fractured2K <fractured2k@gmail.com>
2019-02-11 11:39:00 -0800

    Created/Completed HeaderTitle and HeaderContent

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -1,2 +1,14 @@
 import React from 'react';
 import './Header.css';
+
+function HeaderTitle() {
+    return (
+        <div>
+            <p>
+                Lambda School <span>@LambdaSchool</span> &bullet; <span>26 Jan</span>
+            </p>
+        </div>
+    )
+}
+
+export default HeaderTitle;
