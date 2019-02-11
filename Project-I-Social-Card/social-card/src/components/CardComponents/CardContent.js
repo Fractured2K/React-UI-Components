d43e1e28e53ca729922dedd798c602ba66d4d1c9
Fractured2K <fractured2k@gmail.com>
2019-02-11 11:56:41 -0800

    Completed CardContent Component

--- Project-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
+++ Project-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
@@ -2,7 +2,13 @@ import React from 'react';
 import './Card.css';
 
 function CardContent() {
-
+    return (
+        <div>
+            <h1>Get Started with React</h1>
+            <p>React makes it painless to create interactive UIs. Design simple views for each state in your application</p>
+            <p>reactjs.org</p>
+        </div>
+    )
 }
 
 export default CardContent;
