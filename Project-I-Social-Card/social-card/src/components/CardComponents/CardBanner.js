4052d9446473882a282028f5d64dd0742e310bc0
Fractured2K <fractured2k@gmail.com>
2019-02-11 11:46:57 -0800

    Created CardContainer, CardBanner and CardContent Components

--- Project-I-Social-Card/social-card/src/components/CardComponents/CardBanner.js
+++ Project-I-Social-Card/social-card/src/components/CardComponents/CardBanner.js
@@ -1,2 +1,8 @@
 import React from 'react';
 import './Card.css';
+
+function CardBanner() {
+
+}
+
+export default CardBanner;
--- Project-I-Social-Card/social-card/src/components/CardComponents/CardContainer.js
+++ Project-I-Social-Card/social-card/src/components/CardComponents/CardContainer.js
@@ -1,2 +1,8 @@
 import React from 'react';
 import './Card.css';
+
+function CardContainer() {
+
+}
+
+export default CardContainer;
