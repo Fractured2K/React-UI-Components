6138c047a67d4c275e32e0776d333a83ce7ec742
Fractured2K <fractured2k@gmail.com>
2019-02-12 11:28:31 -0800

    Completed ButtonContainer.js

---
+++ Project-II-Calculator/calculator/src/components/ButtonComponents/ButtonContainer.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import './ButtonContainer.css';
+
+ButtonContainer = props => <div className={props.buttonContainer}>{props.children}</div>
+
+export default ButtonContainer;
